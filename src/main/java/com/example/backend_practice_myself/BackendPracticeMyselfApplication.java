b7bafcf56650fd5bbe90e1a7f8767535215d3083
ChaeYoonUm <109208182+ChaeYoonUm@users.noreply.github.com>
2023-03-16 23:12:11 +0900

    Initial commit

---
+++ src/main/java/com/example/backend_practice_myself/BackendPracticeMyselfApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_practice_myself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPracticeMyselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPracticeMyselfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_practice_myself/BackendPracticeMyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_practice_myself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPracticeMyselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
