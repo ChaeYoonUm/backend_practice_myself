89ef2b01ae5d0fd7898f73273d907773440dc842
ChaeYoonUm <109208182+ChaeYoonUm@users.noreply.github.com>
2023-03-20 15:16:27 +0900

    Chapter mySQL conntection

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -27,7 +27,7 @@ var main = {
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
-            alert('글이 등록되었습니다.');
+            alert('Content has been uploaded');
             window.location.href = '/'; // 글 등록이 성공하면 메인페이지(/)로 이동
         }).fail(function (error) {
             alert(JSON.stringify(error));
@@ -48,7 +48,7 @@ var main = {
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
-            alert('글이 수정되었습니다.');
+            alert('Content has been updated');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
@@ -63,7 +63,7 @@ var main = {
             dataType: 'json',
             contentType:'application/json; charset=utf-8'
         }).done(function() {
-            alert('글이 삭제되었습니다.');
+            alert('Content has been deleted');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
